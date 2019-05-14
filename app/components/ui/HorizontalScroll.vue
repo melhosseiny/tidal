7c8c3d9307e57ff124a243c1baa2cf3a6b96268b
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2019-05-14 02:00:25 +0200

    Possibly fix Safari GIF issue

--- app/components/ui/HorizontalScroll.vue
+++ app/components/ui/HorizontalScroll.vue
@@ -103,7 +103,7 @@
         console.log("reset", lazyImages);
         let lazyImageObserver = this.lazyImageObserver;
         lazyImages.forEach(function(lazyImage) {
-          lazyImage.src = "data:image/gif;base64,R0lGODlhqgCqAAAAACH5BAEAAAEALAAAAACqAKoAADs=";
+          lazyImage.src = "data:image/gif;base64,R0lGODlhqgCqAAAAACH5BAEAAAAALAAAAACqAKoAAAICTAEAOw==";
           lazyImage.classList.remove("lazy-loaded");
           lazyImage.classList.add("lazy");
           lazyImageObserver.unobserve(lazyImage);
